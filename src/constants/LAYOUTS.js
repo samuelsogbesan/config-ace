dbfbd5a8ba26bce5eaeca5d479db68233cb42777
samsog <juwonsogbesan@gmail.com>
2021-02-28 12:25:30 +0000

    Export QWERTY Keyboard format ⌨️

---
+++ src/constants/LAYOUTS.js
@@ -0,0 +1,12 @@
+const layouts = {}
+
+layouts.QWERTY = [
+  "!esc","f1","f2","f3","f4","f5","f6","f7","f8","f9","f10","f11","f12","!💾",
+ "1","2","3","4","5","6","7","8","9","0","kp_minus","kp_plus",
+ "backspace","tab","q","w","e","r","t","y","u","i","o","p","[","]","/",
+ "capslock","a","s","d","f","g","h","j","k","l",";","'","enter",
+ "shift","\\","z","x","c","v","b","n","m",",",".","rshift","ctrl","alt","space","ralt","rctrl"
+];
+
+Object.freeze(layouts);
+module.exports = layouts;
