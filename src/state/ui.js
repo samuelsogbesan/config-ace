a8669b21f872070062f748922ad3bfa34f54ec44
samsog <juwonsogbesan@gmail.com>
2021-03-10 14:32:31 +0000

    Add Hooks for Toasts

--- src/state/ui.js
+++ src/state/ui.js
@@ -2,7 +2,8 @@
  * Basic UI Manager
  */
 const UITargets = {
-  Tray: document.getElementById('search-results-form')
+  Tray: document.getElementById('search-results-form'),
+  InstructionBox: document.getElementById('instruction-box')
 }
 
 const UIManagementTools = {}
@@ -12,5 +13,24 @@ UIManagementTools.closeTray = () => UITargets.Tray.classList.add('hidden');
 UIManagementTools.openTray = () => UITargets.Tray.classList.remove('hidden');
 
 UIManagementTools.submitSearch = () => document.getElementById('main-submit').click();
+
+UIManagementTools.warnToast = (instruction) => {
+  UITargets.InstructionBox.classList.remove('hint');
+  UITargets.InstructionBox.classList.add('warm');
+  UITargets.InstructionBox.innerHTML = instruction;
+}
+
+UIManagementTools.hintToast = (instruction) => {
+  UITargets.InstructionBox.classList.remove('warn');
+  UITargets.InstructionBox.classList.add('hint');
+  UITargets.InstructionBox.innerHTML = instruction;
+}
+
+UIManagementTools.flashToast = (message, oldMessage = UITargets.InstructionBox.innerHTML) => {
+  UITargets.InstructionBox.textContent = message;
+  setTimeout(() => {
+    UITargets.InstructionBox.textContent = oldMessage;
+  }, 600);
+}
 
 module.exports = UIManagementTools;
