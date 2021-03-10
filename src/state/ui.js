a0ad943174e933c1eabf5d50a4ac9e893629ad7c
samsog <juwonsogbesan@gmail.com>
2021-03-10 15:52:47 +0000

    Update & Clear Bind Counters

--- src/state/ui.js
+++ src/state/ui.js
@@ -1,9 +1,12 @@
+const ConfigState = require('./config');
+
 /**
  * Basic UI Manager
  */
 const UITargets = {
   Tray: document.getElementById('search-results-form'),
-  InstructionBox: document.getElementById('instruction-box')
+  InstructionBox: document.getElementById('instruction-box'),
+  Keyboard: document.getElementById('keyboard')
 }
 
 const UIManagementTools = {}
@@ -31,6 +34,22 @@ UIManagementTools.flashToast = (message, oldMessage = UITargets.InstructionBox.i
   setTimeout(() => {
     UITargets.InstructionBox.textContent = oldMessage;
   }, 600);
+}
+
+UIManagementTools.refreshBindCounter = (bind) => {
+  const bindCount = ConfigState.getBind(bind).length;
+  const selector = `.key[data-bindcode="${bind}"]`;
+  const keyElement = document.querySelector(selector);
+  keyElement.setAttribute('data-bindcount', bindCount);
+}
+
+UIManagementTools.clearBindCounters = () => {
+  let keys = UITargets.Keyboard.children;
+
+  for (var i = 0; i < keys.length; i++) {
+    let key = keys[i];
+    key.removeAttribute('data-bindcount');
+  }
 }
 
 module.exports = UIManagementTools;
