230d09dc1dc713ea25b08a5198bbe3d2a0936796
samsog <juwonsogbesan@gmail.com>
2021-04-07 01:03:58 +0100

    Abstract save functionality into utility

---
+++ utils/save.js
@@ -0,0 +1,11 @@
+const ConfigState = require("../state/config");
+
+export const save = async () => {
+  const blobParts = ConfigState.export();
+  blobParts.unshift('// Config Courtesy of "Easy Config", a CSGO Config Generator by @sam.sog 🥳\n');
+  const file = new Blob(blobParts, {endings: 'native'});
+  const a = document.createElement('a');
+  a.download='config.cfg'
+  a.href = URL.createObjectURL(file);
+  a.click();
+}
