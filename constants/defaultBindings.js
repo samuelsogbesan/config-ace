7b0c06d9a55146ae8204049793607037c1110308
samsog <juwonsogbesan@gmail.com>
2021-04-08 02:06:17 +0100

    Default CSGO bindings

---
+++ constants/defaultBindings.js
@@ -0,0 +1,198 @@
+module.exports = {
+  "0": [
+    {
+      "command": "slot10",
+      "value": ""
+    }
+  ],
+  "1": [
+    {
+      "command": "slot1",
+      "value": ""
+    }
+  ],
+  "2": [
+    {
+      "command": "slot2",
+      "value": ""
+    }
+  ],
+  "3": [
+    {
+      "command": "slot3",
+      "value": ""
+    }
+  ],
+  "4": [
+    {
+      "command": "slot4",
+      "value": ""
+    }
+  ],
+  "5": [
+    {
+      "command": "slot5",
+      "value": ""
+    }
+  ],
+  "6": [
+    {
+      "command": "slot6",
+      "value": ""
+    }
+  ],
+  "7": [
+    {
+      "command": "slot7",
+      "value": ""
+    }
+  ],
+  "8": [
+    {
+      "command": "slot8",
+      "value": ""
+    }
+  ],
+  "9": [
+    {
+      "command": "slot9",
+      "value": ""
+    }
+  ],
+  "a": [
+    {
+      "command": "+moveleft",
+      "value": ""
+    }
+  ],
+  "b": [
+    {
+      "command": "buymenu",
+      "value": ""
+    }
+  ],
+  "c": [
+    {
+      "command": "use",
+      "value": " weapon_flashbang"
+    }
+  ],
+  "d": [
+    {
+      "command": "+moveright",
+      "value": ""
+    }
+  ],
+  "e": [
+    {
+      "command": "+use",
+      "value": ""
+    }
+  ],
+  "f": [
+    {
+      "command": "+lookatweapon",
+      "value": ""
+    }
+  ],
+  "g": [
+    {
+      "command": "drop",
+      "value": ""
+    }
+  ],
+  "i": [
+    {
+      "command": "toggle",
+      "value": " mat_monitorgamma 1.6 2.2"
+    }
+  ],
+  "m": [
+    {
+      "command": "teammenu",
+      "value": ""
+    }
+  ],
+  "o": [
+    {
+      "command": "toggle",
+      "value": " cl_crosshairsize 9999 2"
+    }
+  ],
+  "p": [
+    {
+      "command": "use",
+      "value": " weapon_c4"
+    },
+    {
+      "command": "drop",
+      "value": ""
+    }
+  ],
+  "q": [
+    {
+      "command": "lastinv",
+      "value": ""
+    }
+  ],
+  "r": [
+    {
+      "command": "+reload",
+      "value": ""
+    }
+  ],
+  "s": [
+    {
+      "command": "+back",
+      "value": ""
+    }
+  ],
+  "t": [
+    {
+      "command": "+spray_menu",
+      "value": ""
+    }
+  ],
+  "u": [
+    {
+      "command": "messagemode2",
+      "value": ""
+    }
+  ],
+  "v": [
+    {
+      "command": "use",
+      "value": " weapon_hegrenade"
+    }
+  ],
+  "w": [
+    {
+      "command": "+forward",
+      "value": ""
+    }
+  ],
+  "x": [
+    {
+      "command": "use",
+      "value": " weapon_smokegrenade"
+    }
+  ],
+  "y": [
+    {
+      "command": "messagemode",
+      "value": ""
+    }
+  ],
+  "z": [
+    {
+      "command": "use",
+      "value": " weapon_incgrenade"
+    }
+  ],
+  "kp_slash": [
+    {
+      "command": "toggle",
+      "value": " gameinstructor_enable 1 0"
+    }
+  ]
+}
