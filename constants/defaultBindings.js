04cd22b15872f4c183b0446474c4e4a40c22d00b
samsog <juwonsogbesan@gmail.com>
2021-04-21 19:42:32 +0100

    [🔨] Migrate to new cached config format

--- constants/defaultBindings.js
+++ constants/defaultBindings.js
@@ -1,198 +1,195 @@
 module.exports = {
-  "0": [
-    {
-      "command": "slot10",
-      "value": ""
-    }
-  ],
-  "1": [
-    {
-      "command": "slot1",
-      "value": ""
-    }
-  ],
-  "2": [
-    {
-      "command": "slot2",
-      "value": ""
-    }
-  ],
-  "3": [
-    {
-      "command": "slot3",
-      "value": ""
-    }
-  ],
-  "4": [
-    {
-      "command": "slot4",
-      "value": ""
-    }
-  ],
-  "5": [
-    {
-      "command": "slot5",
-      "value": ""
-    }
-  ],
-  "6": [
-    {
-      "command": "slot6",
-      "value": ""
-    }
-  ],
-  "7": [
-    {
-      "command": "slot7",
-      "value": ""
-    }
-  ],
-  "8": [
-    {
-      "command": "slot8",
-      "value": ""
-    }
-  ],
-  "9": [
-    {
-      "command": "slot9",
-      "value": ""
-    }
-  ],
-  "a": [
-    {
-      "command": "+moveleft",
-      "value": ""
-    }
-  ],
-  "b": [
-    {
-      "command": "buymenu",
-      "value": ""
-    }
-  ],
-  "c": [
-    {
-      "command": "use",
-      "value": " weapon_flashbang"
-    }
-  ],
-  "d": [
-    {
-      "command": "+moveright",
-      "value": ""
-    }
-  ],
-  "e": [
-    {
-      "command": "+use",
-      "value": ""
-    }
-  ],
-  "f": [
-    {
-      "command": "+lookatweapon",
-      "value": ""
-    }
-  ],
-  "g": [
-    {
-      "command": "drop",
-      "value": ""
-    }
-  ],
-  "i": [
-    {
-      "command": "toggle",
-      "value": " mat_monitorgamma 1.6 2.2"
-    }
-  ],
-  "m": [
-    {
-      "command": "teammenu",
-      "value": ""
-    }
-  ],
-  "o": [
-    {
-      "command": "toggle",
-      "value": " cl_crosshairsize 9999 2"
-    }
-  ],
-  "p": [
-    {
-      "command": "use",
-      "value": " weapon_c4"
+  "0": {
+    "_meta": {
+      "bindType": "bind"
     },
-    {
-      "command": "drop",
-      "value": ""
-    }
-  ],
-  "q": [
-    {
-      "command": "lastinv",
-      "value": ""
-    }
-  ],
-  "r": [
-    {
-      "command": "+reload",
-      "value": ""
-    }
-  ],
-  "s": [
-    {
-      "command": "+back",
-      "value": ""
-    }
-  ],
-  "t": [
-    {
-      "command": "+spray_menu",
-      "value": ""
-    }
-  ],
-  "u": [
-    {
-      "command": "messagemode2",
-      "value": ""
-    }
-  ],
-  "v": [
-    {
-      "command": "use",
-      "value": " weapon_hegrenade"
-    }
-  ],
-  "w": [
-    {
-      "command": "+forward",
-      "value": ""
-    }
-  ],
-  "x": [
-    {
-      "command": "use",
-      "value": " weapon_smokegrenade"
-    }
-  ],
-  "y": [
-    {
-      "command": "messagemode",
-      "value": ""
-    }
-  ],
-  "z": [
-    {
-      "command": "use",
-      "value": " weapon_incgrenade"
-    }
-  ],
-  "kp_slash": [
-    {
-      "command": "toggle",
-      "value": " gameinstructor_enable 1 0"
-    }
-  ]
+    "slot10": ""
+  },
+  "1": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "slot1": ""
+  },
+  "2": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "slot2": ""
+  },
+  "3": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "slot3": ""
+  },
+  "4": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "slot4": ""
+  },
+  "5": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "slot5": ""
+  },
+  "6": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "slot6": ""
+  },
+  "7": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "slot7": ""
+  },
+  "8": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "slot8": ""
+  },
+  "9": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "slot9": ""
+  },
+  "a": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "+moveleft": ""
+  },
+  "b": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "buymenu": ""
+  },
+  "c": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "use": ""
+  },
+  "d": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "+moveright": ""
+  },
+  "e": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "+use": ""
+  },
+  "f": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "+lookatweapon": ""
+  },
+  "g": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "drop": ""
+  },
+  "i": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "toggle": ""
+  },
+  "m": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "teammenu": ""
+  },
+  "o": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "toggle": ""
+  },
+  "p": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "use": "",
+    "drop": ""
+  },
+  "q": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "lastinv": ""
+  },
+  "r": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "+reload": ""
+  },
+  "s": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "+back": ""
+  },
+  "t": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "+spray_menu": ""
+  },
+  "u": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "messagemode2": ""
+  },
+  "v": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "use": ""
+  },
+  "w": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "+forward": ""
+  },
+  "x": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "use": ""
+  },
+  "y": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "messagemode": ""
+  },
+  "z": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "use": ""
+  },
+  "kp_slash": {
+    "_meta": {
+      "bindType": "bind"
+    },
+    "toggle": ""
+  }
 }
