643659d8559cac61d3f3653602e3df70ae3b442d
samsog <juwonsogbesan@gmail.com>
2021-04-22 14:35:57 +0100

    Update Config Credit

--- utils/save.js
+++ utils/save.js
@@ -2,7 +2,7 @@ const ConfigState = require("../state/config");
 
 export const save = () => {
   const blobParts = ConfigState.export();
-  blobParts.unshift('// Config Courtesy of "Easy Config", a CSGO Config Generator by @sam.sog 🥳\n');
+  blobParts.unshift('// Config Courtesy of "Config Ace", a CSGO Config Generator at https://configace.com 🥳\n');
   const file = new Blob(blobParts, {endings: 'native'});
   const a = document.createElement('a');
   a.download='config.cfg'
